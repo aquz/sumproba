2c74893d781bfc9ad82c72f16ca8431f27d18dec
Mateusz Terlikowski <mterlikowski95@gmail.com>
2019-06-05 22:23:27 +0200

    fix file send

--- email.js
+++ email.js
@@ -1,107 +1,121 @@
-const mongoose = require("mongoose")
-const nodeMailer = require('nodemailer')
-mongoose.Promise = global.Promise
-mongoose.connect('mongodb://localhost:27017/sumtransfer123', { useNewUrlParser: true })
+const mongoose = require("mongoose");
+const nodeMailer = require("nodemailer");
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost:27017/sumtransfer123", {
+  useNewUrlParser: true
+});
 
 const nameSchema = new mongoose.Schema({
-    receiver: String,
-    sender: String,
-    subject: String,
-    upfile: String,
-    message: String
-})
+  receiver: String,
+  sender: String,
+  subject: String,
+  upfile: String,
+  message: String
+});
 
 const transporter = nodeMailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
-    auth: {
-        user: 'sum.transfer@gmail.com',
-        pass: 'tymczasowe2019'
-    }
-})
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: "sum.transfer@gmail.com",
+    pass: "tymczasowe2019"
+  }
+});
 
-const baza_danych = mongoose.model('sum_baza', nameSchema)
+const baza_danych = mongoose.model("sum_baza", nameSchema);
 
-const create_options_for_sender = ({ receiver, sender, subject, file, message, body }) => {
-    const html = `
+const create_options_for_sender = ({ files, body }) => {
+  const { receiver, sender, subject, message } = body;
+  const { upfile } = files;
+
+  const html = `
         <p> Wiadomość z formularza </p>
         <ul>
-            <li>Mail do: ${ receiver}</li>
-            <li>Mail od: ${ sender}</li>
-            <li>Mail: ${ subject}</li>
+            <li>Mail do: ${receiver}</li>
+            <li>Mail od: ${sender}</li>
+            <li>Mail: ${subject}</li>
         </ul>
         <h3> Wiadomość </h3>
-        <p> Wysłałeś plik z wiadomością ${ message} </p>
+        <p> Wysłałeś plik z wiadomością ${message} </p>
         <p> Pozostała część wiadomości </p>
-    `
+    `;
 
-    return {
-        from: '"SUM Transfer" <sum.transfer@gmail.com>',
-        text: body,
-        to: receiver,
-        subject, file, html,
-    }
-}
+  return {
+    from: '"SUM Transfer" <sum.transfer@gmail.com>',
+    text: html,
+    to: sender,
+    subject,
+    attachments: [{ filename: upfile.name, content: upfile.data }],
+    html
+  };
+};
 
+const create_options_for_receiver = ({ files, body }) => {
+  const { receiver, sender, subject, message } = body;
+  const { upfile } = files;
 
-const create_options_for_receiver = ({ upfile, receiver, sender, subject, file, message, body }) => {
-    const html = `
+  const html = `
         <p> Wiadomość z formularza </p>
         <ul>
-            <li>Mail do:  ${ receiver}</li>
-            <li>Mail od: ${ sender}</li>
-            <li>Mail: ${ subject}</li>
+            <li>Mail do:  ${receiver}</li>
+            <li>Mail od: ${sender}</li>
+            <li>Mail: ${subject}</li>
         </ul>
         <h3> Wiadomość </h3>
-        <p> Otrzymałeś plik z wiadomością ${ message} </p>
-        <p> Nazwa pliku ${ upfile} </p>
+        <p> Otrzymałeś plik z wiadomością ${message} </p>
+        <p> Nazwa pliku ${
+          upfile.name
+        } lub <a href="http://example.com/uploads/${upfile.name}">klik</a> </p>
         <p> Pozostała część wiadomości </p>
-    `
-    return {
-        from: '"SUM Transfer" <sum.transfer@gmail.com>',
-        text: body,
-        to: sender,
-        subject, file, html,
-    }
-}
+    `;
+  return {
+    from: '"SUM Transfer" <sum.transfer@gmail.com>',
+    text: html,
+    to: receiver,
+    subject,
+    attachments: [{ filename: upfile.name, content: upfile.data }],
+    html
+  };
+};
 
-const send_to_sender = (body) => new Promise((resolve, reject) => {
-    transporter.sendMail(create_options_for_sender(body), (error, info) => {
-        if (error) {
-            reject(error)
-        } else {
-            console.log('Message %s sent: %s', info.messageId, info.response)
-            resolve()
-        }
-    })
-})
+const send_to_sender = req =>
+  new Promise((resolve, reject) => {
+    transporter.sendMail(create_options_for_sender(req), (error, info) => {
+      if (error) {
+        reject(error);
+      } else {
+        console.log("Message %s sent: %s", info.messageId, info.response);
+        resolve();
+      }
+    });
+  });
 
+const send_to_receiver = req =>
+  new Promise((resolve, reject) => {
+    transporter.sendMail(create_options_for_receiver(req), (error, info) => {
+      if (error) {
+        reject(error);
+      } else {
+        console.log("Message %s sent: %s", info.messageId, info.response);
+        resolve();
+      }
+    });
+  });
 
-const send_to_receiver = (body) => new Promise((resolve, reject) => {
-    transporter.sendMail(create_options_for_receiver(body), (error, info) => {
-        if (error) {
-            reject(error)
-        } else {
-            console.log('Message %s sent: %s', info.messageId, info.response)
-            resolve()
-        }
-    })
-})
-
-const save_to_db = body => new Promise((resolve, reject) => {
-    const myData = new baza_danych(body)
+const save_to_db = body =>
+  new Promise((resolve, reject) => {
+    const myData = new baza_danych(body);
 
     myData
-        .save()
-        .then(item => resolve('Zapisano prawidłowo do bazy'))
-        .catch(err => reject('Wystąpił błąd podczas zapisu do bazy danych'))
-})
+      .save()
+      .then(item => resolve("Zapisano prawidłowo do bazy"))
+      .catch(err => reject("Wystąpił błąd podczas zapisu do bazy danych"));
+  });
 
-
-
-module.exports = body => Promise.all([
-    send_to_receiver(body),
-    send_to_sender(body),
-    save_to_db(body),
-])
+module.exports = req =>
+  Promise.all([
+    send_to_receiver(req),
+    send_to_sender(req),
+    save_to_db(req.body)
+  ]);
--- server.js
+++ server.js
@@ -1,34 +1,35 @@
-const express = require('express')
-const app = express()
-const http = require('http')
-const upload = require('express-fileupload')
-const bodyParser = require('body-parser')
-const handleUpload = require('./upload')
-const handleEmail = require('./email')
+const express = require("express");
+const app = express();
+const http = require("http");
+const upload = require("express-fileupload");
+const bodyParser = require("body-parser");
+const handleUpload = require("./upload");
+const handleEmail = require("./email");
 var port = 3000;
 
-app.set('view engine', 'ejs')
+app.set("view engine", "ejs");
 
-app.use(express.static('public'))
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
-app.use(upload())
+app.use(express.static("public"));
+app.use("/uploads/", express.static("uploads"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(upload());
 
-app.get('/', (req, res) => res.render('index'))
+app.get("/", (req, res) => res.render("index"));
 
-app.post('/send-email', (req, res) => new Promise((resolve, reject) => {
-    Promise
-        .all([
-            handleUpload(req.files),
-            handleEmail(req.body),
-        ])
-        .then(() => res.render('success'))
+app.post(
+  "/send-email",
+  (req, res) =>
+    new Promise((resolve, reject) => {
+      Promise.all([handleUpload(req.files), handleEmail(req)])
+        .then(() => res.render("success"))
         .catch(error => {
-            console.log(error)
-            res.render('error')
-        })
-}))
+          console.log(error);
+          res.render("error");
+        });
+    })
+);
 
 app.listen(port, () => {
-    console.log("Server listening on port " + port);
+  console.log("Server listening on port " + port);
 });
